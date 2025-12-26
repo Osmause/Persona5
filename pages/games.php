d5fe93b2ccea8b473f0792c769534c92b0586f46
mattheo-bils <mattheobils@gmail.com>
2025-12-26 14:40:44 +0100

    debut très lent

---
+++ pages/games.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../asset/css/pages/games.css">
+</head>
+<body>
+    <?php include('./includes/header.html'); ?>
+    <main>
+        <section>
+            <img src="..\assets\css\img\png\background-persona5.jpg" alt="">
+            <h1>Games reviews</h1>
+            <p></p>
+        </section>
+        <section>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </section>
+    </main>
+    <?php include('./includes/footer.html'); ?>
+</body>
+</html>
