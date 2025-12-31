b9f2be3059eb99272e024f13f636c5246b940c1a
mattheo-bils <mattheobils@gmail.com>
2025-12-31 11:16:57 +0100

    html et css commencé

---
+++ pages/forum.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/pages/forum.css">
+</head>
+<body>
+    <?php include('../includes/header.html'); ?>
+    <main>
+        <section>
+            <h1>Community</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec <br>malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum.</p>
+        </section>
+        <section>
+            <div>
+                <h2>All Members</h2>
+                <div>
+                    <p>Show</p>
+                    <input type="">
+                </div>
+            </div>
+            <section>
+                <div>
+                    <img src="" alt="">
+                    <div>
+                        <div>
+                            <p>James Smith</p>
+                            <p>a publié</p>
+                        </div>
+                        <p>June 21, 2018</p>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, cdictum nisl onsectetur adipisc ing ipsum dolor sit ame. Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor <br>sit ame.Donec venenatis at eros sit amet aliquam. Donec vel orci efficitur, dictum nisl vitae, scelerisque nibh. Curabitur eget ipsum pulvinar nunc <br>gravida interdum.</p>
+            </section>
+            <section>
+                <div>
+                    <img src="" alt="">
+                    <div>
+                        <div>
+                            <p>Partik Williams</p>
+                            <p>a publié</p>
+                        </div>
+                        <p>June 21, 2018</p>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, cdictum nisl onsectetur adipisc ing ipsum dolor sit ame. Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor <br>sit ame.Donec venenatis at eros sit amet aliquam. Donec vel orci efficitur, dictum nisl vitae, scelerisque nibh. Curabitur eget ipsum pulvinar nunc <br>gravida interdum.</p>
+            </section>
+            <section>
+                <div>
+                    <img src="" alt="">
+                    <div>
+                        <div>
+                            <p>Cris The Man</p>
+                            <p>a publié</p>
+                        </div>
+                        <p>June 21, 2018</p>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, cdictum nisl onsectetur adipisc ing ipsum dolor sit ame. Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor <br>sit ame.Donec venenatis at eros sit amet aliquam. Donec vel orci efficitur, dictum nisl vitae, scelerisque nibh. Curabitur eget ipsum pulvinar nunc <br>gravida interdum.</p>
+            </section>
+            <section>
+                <div>
+                    <img src="" alt="">
+                    <div>
+                        <div>
+                            <p>Maria Doe</p>
+                            <p>a publié</p>
+                        </div>
+                        <p>June 21, 2018</p>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, cdictum nisl onsectetur adipisc ing ipsum dolor sit ame. Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor <br>sit ame.Donec venenatis at eros sit amet aliquam. Donec vel orci efficitur, dictum nisl vitae, scelerisque nibh. Curabitur eget ipsum pulvinar nunc <br>gravida interdum.</p>
+            </section>
+            <section>
+                <div>
+                    <img src="" alt="">
+                    <div>
+                        <div>
+                            <p>Michael James</p>
+                            <p>a publié</p>
+                        </div>
+                        <p>June 21, 2018</p>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, cdictum nisl onsectetur adipisc ing ipsum dolor sit ame. Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor <br>sit ame.Donec venenatis at eros sit amet aliquam. Donec vel orci efficitur, dictum nisl vitae, scelerisque nibh. Curabitur eget ipsum pulvinar nunc <br>gravida interdum.</p>
+            </section>
+            <section class="forum-pages">
+                <button>01.</button>
+                <button>02.</button>
+                <button>03.</button>
+            </section>
+        </section>
+    </main>
+    <?php include('../includes/footer.html'); ?>
+</body>
+</html>
