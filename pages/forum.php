d6cd9e681fdd6583f9b2dac4d85d56091be2503b
mattheo-bils <mattheobils@gmail.com>
2025-12-31 12:07:39 +0100

    avancé

--- pages/forum.php
+++ pages/forum.php
@@ -14,82 +14,84 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec <br>malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum.</p>
         </section>
         <section>
-            <div>
-                <h2>All Members</h2>
-                <div>
-                    <p>Show</p>
-                    <input type="">
-                </div>
-            </div>
-            <section>
-                <div>
-                    <img src="" alt="">
-                    <div>
-                        <div>
-                            <p>James Smith</p>
-                            <p>a publié</p>
-                        </div>
-                        <p>June 21, 2018</p>
+            <section class="full-forum">
+                <div class="all-member">
+                    <h2>All Members</h2>
+                    <div class="filter">
+                        <p>Show</p>
+                        <input type="">
                     </div>
                 </div>
-                <p>Lorem ipsum dolor sit amet, cdictum nisl onsectetur adipisc ing ipsum dolor sit ame. Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor <br>sit ame.Donec venenatis at eros sit amet aliquam. Donec vel orci efficitur, dictum nisl vitae, scelerisque nibh. Curabitur eget ipsum pulvinar nunc <br>gravida interdum.</p>
-            </section>
-            <section>
-                <div>
-                    <img src="" alt="">
+                <section class="forum-message">
                     <div>
+                        <img src="" alt="">
                         <div>
-                            <p>Partik Williams</p>
-                            <p>a publié</p>
+                            <div>
+                                <p>James Smith</p>
+                                <p>a publié</p>
+                            </div>
+                            <p>June 21, 2018</p>
                         </div>
-                        <p>June 21, 2018</p>
                     </div>
-                </div>
-                <p>Lorem ipsum dolor sit amet, cdictum nisl onsectetur adipisc ing ipsum dolor sit ame. Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor <br>sit ame.Donec venenatis at eros sit amet aliquam. Donec vel orci efficitur, dictum nisl vitae, scelerisque nibh. Curabitur eget ipsum pulvinar nunc <br>gravida interdum.</p>
-            </section>
-            <section>
-                <div>
-                    <img src="" alt="">
+                    <p>Lorem ipsum dolor sit amet, cdictum nisl onsectetur adipisc ing ipsum dolor sit ame. Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor <br>sit ame.Donec venenatis at eros sit amet aliquam. Donec vel orci efficitur, dictum nisl vitae, scelerisque nibh. Curabitur eget ipsum pulvinar nunc <br>gravida interdum.</p>
+                </section>
+                <section class="forum-message">
                     <div>
+                        <img src="" alt="">
                         <div>
-                            <p>Cris The Man</p>
-                            <p>a publié</p>
+                            <div>
+                                <p>Partik Williams</p>
+                                <p>a publié</p>
+                            </div>
+                            <p>June 21, 2018</p>
                         </div>
-                        <p>June 21, 2018</p>
                     </div>
-                </div>
-                <p>Lorem ipsum dolor sit amet, cdictum nisl onsectetur adipisc ing ipsum dolor sit ame. Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor <br>sit ame.Donec venenatis at eros sit amet aliquam. Donec vel orci efficitur, dictum nisl vitae, scelerisque nibh. Curabitur eget ipsum pulvinar nunc <br>gravida interdum.</p>
-            </section>
-            <section>
-                <div>
-                    <img src="" alt="">
+                    <p>Lorem ipsum dolor sit amet, cdictum nisl onsectetur adipisc ing ipsum dolor sit ame. Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor <br>sit ame.Donec venenatis at eros sit amet aliquam. Donec vel orci efficitur, dictum nisl vitae, scelerisque nibh. Curabitur eget ipsum pulvinar nunc <br>gravida interdum.</p>
+                </section>
+                <section class="forum-message">
                     <div>
+                        <img src="" alt="">
                         <div>
-                            <p>Maria Doe</p>
-                            <p>a publié</p>
+                            <div>
+                                <p>Cris The Man</p>
+                                <p>a publié</p>
+                            </div>
+                            <p>June 21, 2018</p>
                         </div>
-                        <p>June 21, 2018</p>
                     </div>
-                </div>
-                <p>Lorem ipsum dolor sit amet, cdictum nisl onsectetur adipisc ing ipsum dolor sit ame. Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor <br>sit ame.Donec venenatis at eros sit amet aliquam. Donec vel orci efficitur, dictum nisl vitae, scelerisque nibh. Curabitur eget ipsum pulvinar nunc <br>gravida interdum.</p>
-            </section>
-            <section>
-                <div>
-                    <img src="" alt="">
+                    <p>Lorem ipsum dolor sit amet, cdictum nisl onsectetur adipisc ing ipsum dolor sit ame. Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor <br>sit ame.Donec venenatis at eros sit amet aliquam. Donec vel orci efficitur, dictum nisl vitae, scelerisque nibh. Curabitur eget ipsum pulvinar nunc <br>gravida interdum.</p>
+                </section>
+                <section class="forum-message">
                     <div>
+                        <img src="" alt="">
                         <div>
-                            <p>Michael James</p>
-                            <p>a publié</p>
+                            <div>
+                                <p>Maria Doe</p>
+                                <p>a publié</p>
+                            </div>
+                            <p>June 21, 2018</p>
                         </div>
-                        <p>June 21, 2018</p>
                     </div>
-                </div>
-                <p>Lorem ipsum dolor sit amet, cdictum nisl onsectetur adipisc ing ipsum dolor sit ame. Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor <br>sit ame.Donec venenatis at eros sit amet aliquam. Donec vel orci efficitur, dictum nisl vitae, scelerisque nibh. Curabitur eget ipsum pulvinar nunc <br>gravida interdum.</p>
-            </section>
-            <section class="forum-pages">
-                <button>01.</button>
-                <button>02.</button>
-                <button>03.</button>
+                    <p>Lorem ipsum dolor sit amet, cdictum nisl onsectetur adipisc ing ipsum dolor sit ame. Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor <br>sit ame.Donec venenatis at eros sit amet aliquam. Donec vel orci efficitur, dictum nisl vitae, scelerisque nibh. Curabitur eget ipsum pulvinar nunc <br>gravida interdum.</p>
+                </section>
+                <section class="forum-message">
+                    <div>
+                        <img src="" alt="">
+                        <div>
+                            <div>
+                                <p>Michael James</p>
+                                <p>a publié</p>
+                            </div>
+                            <p>June 21, 2018</p>
+                        </div>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, cdictum nisl onsectetur adipisc ing ipsum dolor sit ame. Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor <br>sit ame.Donec venenatis at eros sit amet aliquam. Donec vel orci efficitur, dictum nisl vitae, scelerisque nibh. Curabitur eget ipsum pulvinar nunc <br>gravida interdum.</p>
+                </section>
+                <section class="forum-pages">
+                    <button>01.</button>
+                    <button>02.</button>
+                    <button>03.</button>
+                </section>
             </section>
         </section>
     </main>
