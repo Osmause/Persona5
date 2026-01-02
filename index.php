8eed954404459d1db417ed8c8d9da4c368101d8c
raphaellebas <raphael.lebas@outlook.fr>
2026-01-02 15:31:33 +0100

    add: new Section + reclass css

--- index.php
+++ index.php
@@ -17,49 +17,49 @@
                 <div class="slides fade">
                     <div class="item set-bg" data-setbg="/assets/css/img/jpg/thumb-1920-888562.jpg">
                         <div class="container">
-                            <h1 class="annoucement-news">Le nouveau jeu <span class="new-article">Persona 5</span> est
+                            <h1 class="text-whitediamond text-h2-media">Le nouveau jeu <span class="text-sun text-h1-media">Persona 5</span> est
                                 disponible !</h1>
-                            <p class="annoucement-news">Plonge dans l’univers stylé de Persona 5, un JRPG culte mêlant
+                            <p class="text-whitediamond text-h3-media">Plonge dans l’univers stylé de Persona 5, un JRPG culte mêlant
                                 action, stratégie et narration intense. Entre vie lycéenne à Tokyo et combats
                                 surnaturels, forme ton équipe, réveille ton pouvoir et vole le cœur des corrompus. Une
                                 expérience inoubliable au rythme d’une bande-son iconique.</p>
-                            <span class="read">
-                                <a href="games/persona5.php">Lire l'article</a>
+                            <span class="read text-whitediamond bg-sun text-h3-media">
+                                <a class="text-h2-media" href="games/persona5.php">Lire l'article</a>
                             </span>
                         </div>
                         <div class="container-dots">
-                            <span class="dot"></span>
-                            <span class="dot"></span>
-                            <span class="dot"></span>
-                            <span class="dot"></span>
-                            <span class="dot"></span>
+                            <span class="bg-whitediamond"></span>
+                            <span class="bg-whitediamond text-whitediamond"></span>
+                            <span class="bg-whitediamond text-whitediamond"></span>
+                            <span class="bg-whitediamond text-whitediamond"></span>
+                            <span class="bg-whitediamond text-whitediamond"></span>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-        <section class="promo">
-            <div class="promo anim-promo">
+        <section class="promo bg-abbysalblack">
+            <div class="anim-promo">
                 <div class="promo-container">
                     <div class="promo-track">
                         <div class="promo-item">
-                            <span class="new feature-bg">Nouveau</span>
-                            <p class="promo-title">Persona 5 est disponible !</p>
+                            <span class="bg-new text-h2-media text-whitediamond feature-bg">Nouveau</span>
+                            <p class="text-whitediamond text-h2-media">Persona 5 est disponible !</p>
                         </div>
 
                         <div class="promo-item">
-                            <span class="update feature-bg">Mise à jour</span>
-                            <p class="promo-title">Persona 5 a subit une mise à jour le 15/11</p>
+                            <span class="bg-sun text-h2-media text-whitediamond feature-bg">Mise à jour</span>
+                            <p class="text-whitediamond text-h2-media">Persona 5 a subit une mise à jour le 15/11</p>
                         </div>
 
                         <div class="promo-item">
-                            <span class="article-prm feature-bg">Article</span>
-                            <p class="promo-title">Persona 5 a fait 1millions de ventes en un mois !</p>
+                            <span class="bg-article text-h2-media text-whitediamond feature-bg">Article</span>
+                            <p class="text-whitediamond text-h2-media">Persona 5 a fait 1millions de ventes en un mois !</p>
                         </div>
 
                         <div class="promo-item">
-                            <span class="promotion feature-bg">Promo</span>
-                            <p class="promo-title">20% de réduction sur toute la saga persona ! </p>
+                            <span class="bg-promotion text-h2-media text-whitediamond feature-bg">Promo</span>
+                            <p class="text-whitediamond text-h2-media">20% de réduction sur toute la saga persona ! </p>
                         </div>
 
                     </div>
@@ -68,14 +68,14 @@
 
         </section>
         <section class="blog">
-            <h1>Blog</h1>
+            <h1 class="text-h1-media">Blog</h1>
             <div class="section-container">
                 <div class="blog-card">
                     <div class="img-presentation">
                         <img class="presentation" src="/assets/css/img/jpg/Persona-5-Voleurs.jpg" alt="">
                         <div class="overlay-presentation">
-                            <span class="new feature-bg-review">Nouveau</span>
-                            <p class="overlay">Lorem Ipsum is simply dummy text of the printing and typesetting
+                            <span class="bg-new text-h2-media text-whitediamond feature-bg-review">Nouveau</span>
+                            <p class="text-whitediamond text-h3-media">Lorem Ipsum is simply dummy text of the printing and typesetting
                                 industry.
                             </p>
                         </div>
@@ -85,8 +85,8 @@
                     <div class="img-presentation">
                         <img class="presentation" src="/assets/css/img/jpg/Persona-5-Voleurs.jpg" alt="">
                         <div class="overlay-presentation">
-                            <span class="new feature-bg-review">Nouveau</span>
-                            <p class="overlay">Lorem Ipsum is simply dummy text of the printing and typesetting
+                            <span class="bg-new text-h2-media text-whitediamond feature-bg-review ">Nouveau</span>
+                            <p class="text-whitediamond text-h3-media">Lorem Ipsum is simply dummy text of the printing and typesetting
                                 industry.
                             </p>
                         </div>
@@ -96,8 +96,8 @@
                     <div class="img-presentation">
                         <img class="presentation" src="/assets/css/img/jpg/Persona-5-Voleurs.jpg" alt="">
                         <div class="overlay-presentation">
-                            <span class="new feature-bg-review">Nouveau</span>
-                            <p class="overlay">Lorem Ipsum is simply dummy text of the printing and typesetting
+                            <span class="bg-new text-h2-media text-whitediamond feature-bg-review">Nouveau</span>
+                            <p class="text-whitediamond text-h3-media">Lorem Ipsum is simply dummy text of the printing and typesetting
                                 industry.
                             </p>
                         </div>
@@ -107,8 +107,8 @@
                     <div class="img-presentation">
                         <img class="presentation" src="/assets/css/img/jpg/Persona-5-Voleurs.jpg" alt="">
                         <div class="overlay-presentation">
-                            <span class="new feature-bg-review">Nouveau</span>
-                            <p class="overlay">Lorem Ipsum is simply dummy text of the printing and typesetting
+                            <span class="bg-new text-h2-media text-whitediamond feature-bg-review">Nouveau</span>
+                            <p class="text-whitediamond text-h3-media">Lorem Ipsum is simply dummy text of the printing and typesetting
                                 industry.
                             </p>
                         </div>
@@ -116,15 +116,77 @@
                 </div>
             </div>
         </section>
-        <section class="review">
-            <h1>Review</h1>
+        <section class="tournament bg-darknight">
+            <h1 class="tournament-title bg-sunrise text-h1-media text-whitediamond">Tournoi</h1>
+            <div class="block-tournament">
+                <div class="container-tournament bg-darkshadow  text-whitediamond">
+                <span class="category-tournament text-h2-media bg-sun">Tournoi Prenium</span>
+                <div class="content">
+                    <div class="content-tournament">
+                        <div class="categoryIMG"
+                            data-setbg="/assets/css/img/jpg/Persona-5-Strikers-PS4-Wallpapers-01.jpg">
+                        </div>
+                        <div class="content-information">
+                            <h1 class="text-sun text-h1-media">Persona 5</h1>
+                            <ul class="container-information">
+                                <li><span class="ctr-left text-h2-media">Date de début</span> 12 Janvier 2026</li>
+                                <li><span class="ctr-left text-h2-media">Date de fin</span> 25 Janvier 2026</li>
+                                <li><span class="ctr-left text-h2-media">Nombre d'équipe</span> 12</li>
+                                <li><span class="ctr-left text-h2-media">Auteur du tournoi</span> Admin</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+                  <div class="container-tournament bg-darkshadow  text-whitediamond">
+                <span class="category-tournament text-h2-media bg-sun">Tournoi Prenium</span>
+                <div class="content">
+                    <div class="content-tournament">
+                        <div class="categoryIMG"
+                            data-setbg="/assets/css/img/jpg/Persona-5-Strikers-PS4-Wallpapers-01.jpg">
+                        </div>
+                        <div class="content-information">
+                            <h1 class="text-sun text-h1-media">Persona 5</h1>
+                            <ul class="container-information">
+                                <li><span class="ctr-left text-h2-media">Date de début</span> 12 Janvier 2026</li>
+                                <li><span class="ctr-left text-h2-media">Date de fin</span> 25 Janvier 2026</li>
+                                <li><span class="ctr-left text-h2-media">Nombre d'équipe</span> 12</li>
+                                <li><span class="ctr-left text-h2-media">Auteur du tournoi</span> Admin</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+             <div class="container-tournament bg-darkshadow  text-whitediamond">
+                <span class="category-tournament text-h2-media bg-sun">Tournoi Prenium</span>
+                <div class="content">
+                    <div class="content-tournament">
+                        <div class="categoryIMG"
+                            data-setbg="/assets/css/img/jpg/Persona-5-Strikers-PS4-Wallpapers-01.jpg">
+                        </div>
+                        <div class="content-information">
+                            <h1 class="text-sun text-h1-media">Persona 5</h1>
+                            <ul class="container-information">
+                                <li><span class="text-h2-media">Date de début</span> 12 Janvier 2026</li>
+                                <li><span class="text-h2-media">Date de fin</span> 25 Janvier 2026</li>
+                                <li><span class="text-h2-media">Nombre d'équipe</span> 12</li>
+                                <li><span class="text-h2-media">Auteur du tournoi</span> Admin</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+            </div>
+        </section>
+        <section class="review bg-selectedgray">
+            <h1 class="text-h1-media">Review</h1>
             <div class="section-container">
                 <div class="review-card">
                     <div class="img-presentation">
                         <img class="presentation" src="/assets/css/img/jpg/Persona-5-Voleurs.jpg" alt="">
                         <div class="overlay-presentation">
-                            <span class="new feature-bg-review">9.5</span>
-                            <p class="overlay">Lorem Ipsum is simply dummy text of the printing and typesetting
+                            <span class="bg-new text-h2-media text-whitediamond feature-bg-review">9.5</span>
+                            <p class="text-whitediamond">Lorem Ipsum is simply dummy text of the printing and typesetting
                                 industry.
                             </p>
                         </div>
@@ -134,8 +196,8 @@
                     <div class="img-presentation">
                         <img class="presentation" src="/assets/css/img/jpg/Persona-5-Voleurs.jpg" alt="">
                         <div class="overlay-presentation">
-                            <span class="new feature-bg-review">4.5</span>
-                            <p class="overlay">Lorem Ipsum is simply dummy text of the printing and typesetting
+                            <span class="bg-new text-h2-media text-whitediamond feature-bg-review">4.5</span>
+                            <p class="text-whitediamond">Lorem Ipsum is simply dummy text of the printing and typesetting
                                 industry.
                             </p>
                         </div>
@@ -145,8 +207,8 @@
                     <div class="img-presentation">
                         <img class="presentation" src="/assets/css/img/jpg/Persona-5-Voleurs.jpg" alt="">
                         <div class="overlay-presentation">
-                            <span class="new feature-bg-review">7.5</span>
-                            <p class="overlay">Lorem Ipsum is simply dummy text of the printing and typesetting
+                            <span class="bg-new text-h2-media text-whitediamond feature-bg-review">7.5</span>
+                            <p class="text-whitediamond">Lorem Ipsum is simply dummy text of the printing and typesetting
                                 industry.
                             </p>
                         </div>
@@ -156,8 +218,8 @@
                     <div class="img-presentation">
                         <img class="presentation" src="/assets/css/img/jpg/Persona-5-Voleurs.jpg" alt="">
                         <div class="overlay-presentation">
-                            <span class="new feature-bg-review">10</span>
-                            <p class="overlay">Lorem Ipsum is simply dummy text of the printing and typesetting
+                            <span class="bg-new text-h2-media text-whitediamond feature-bg-review">10</span>
+                            <p class="text-whitediamond text-h3-media">Lorem Ipsum is simply dummy text of the printing and typesetting
                                 industry.
                             </p>
                         </div>
@@ -165,21 +227,21 @@
                 </div>
             </div>
         </section>
-        <section class="latest">
+        <section class="latest bg-grayshadow">
             <img src="/assets/css/img/png/Atlus_Logo_Thin_Stroke.png" alt="">
-            <p class="latest-para">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-            <div class="latest-comment">
-                <h1 class="latest-title">Dernier article</h1>
+            <p class="text-whitediamond text-h3-media">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            <div class="latest-comment bg-darkshadow">
+                <h1 class="text-whitediamond text-h1-media">Dernier article</h1>
                 <div class="comment-area">
                     <div class="comment">
                         <div class="profile">
                             <img class="pic-profile" src="/assets/css/img/jpg/Persona-5-Voleurs.jpg" alt="">
                         </div>
                         <div class="information">
-                            <span class="date-comment">21 Juin 2025</span>
-                            <p class="latest-para">Lorem Ipsum is simply dummy text of the printing and typesetting
+                            <span class="text-sun text-h2-media">21 Juin 2025</span>
+                            <p class="text-whitediamond text-h3-media">Lorem Ipsum is simply dummy text of the printing and typesetting
                                 industry.</p>
-                            <span class="author">By Admin</span>
+                            <span class="text-selectedgray text-h3-media">By Admin</span>
                         </div>
                     </div>
                 </div>
@@ -188,10 +250,10 @@
                         <img class="pic-profile" src="/assets/css/img/jpg/Persona-5-Voleurs.jpg" alt="">
                     </div>
                     <div class="information">
-                        <span class="date-comment">21 Juin 2025</span>
-                        <p class="latest-para">Lorem Ipsum is simply dummy text of the printing and typesetting
+                        <span class="text-sun text-h2-media">21 Juin 2025</span>
+                        <p class="text-whitediamond text-h3-media">Lorem Ipsum is simply dummy text of the printing and typesetting
                             industry.</p>
-                        <span class="author">By Admin</span>
+                        <span class="text-selectedgray text-h3-media">By Admin</span>
                     </div>
                 </div>
                 <div class="comment">
@@ -199,10 +261,10 @@
                         <img class="pic-profile" src="/assets/css/img/jpg/Persona-5-Voleurs.jpg" alt="">
                     </div>
                     <div class="information">
-                        <span class="date-comment">21 Juin 2025</span>
-                        <p class="latest-para">Lorem Ipsum is simply dummy text of the printing and typesetting
+                        <span class="text-sun text-h2-media">21 Juin 2025</span>
+                        <p class="text-whitediamond text-h3-media">Lorem Ipsum is simply dummy text of the printing and typesetting
                             industry.</p>
-                        <span class="author">By Admin</span>
+                        <span class="text-selectedgray text-h3-media">By Admin</span>
                     </div>
                 </div>
                 <div class="comment">
@@ -210,10 +272,10 @@
                         <img class="pic-profile" src="/assets/css/img/jpg/Persona-5-Voleurs.jpg" alt="">
                     </div>
                     <div class="information">
-                        <span class="date-comment">21 Juin 2025</span>
-                        <p class="latest-para">Lorem Ipsum is simply dummy text of the printing and typesetting
+                        <span class="text-sun text-h2-media">21 Juin 2025</span>
+                        <p class="text-whitediamond text-h3-media">Lorem Ipsum is simply dummy text of the printing and typesetting
                             industry.</p>
-                        <span class="author">By Admin</span>
+                        <span class=" text-selectedgray text-h3-media">By Admin</span>
                     </div>
                 </div>
             </div>
